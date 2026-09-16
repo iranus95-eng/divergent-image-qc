6ad3d7d1deb7feb17e872d2f70db37245983008e
iranus95-eng <iranus95@gmail.com>
2026-09-16 09:49:51 +0700

    Bump billing final layout cache version

--- js/receipt-master-dropdown.js
+++ js/receipt-master-dropdown.js
@@ -8,7 +8,7 @@ function boot(){
   loadOnce('data-claim-legacy-persist','./js/claim-legacy-persist.js?v=20260915-0954');
   loadOnce('data-billing-smart-dropdowns','./js/billing-smart-dropdowns.js?v=20260915-1532');
   loadOnce('data-billing-multi-item','./js/billing-multi-item.js?v=20260915-1540');
-  loadOnce('data-billing-final-layout','./js/billing-final-layout.js?v=20260916-0912');
+  loadOnce('data-billing-final-layout','./js/billing-final-layout.js?v=20260916-0947');
   loadOnce('data-receipt-smart-dropdowns','./js/receipt-smart-dropdowns.js?v=20260915-1532');
   loadOnce('data-home-menu-dedupe','./js/home-menu-dedupe.js?v=20260915-1635');
   loadOnce('data-receipt-signature-fix','./js/receipt-signature-fix.js?v=20260915-1647');
